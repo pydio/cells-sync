87984671374315d2121409009e143b535cb6e8fa
cdujeu <charles@ajaxplorer.info>
2019-05-03 13:41:07 +0200

    Use office UI components

--- app/ux/src/App.js
+++ app/ux/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Sockette from 'sockette'
+import SyncTask from './SyncTask'
+import {ScrollablePane} from 'office-ui-fabric-react/lib/ScrollablePane'
+import {Sticky, StickyPositionType} from 'office-ui-fabric-react/lib/Sticky'
+import { Modal } from 'office-ui-fabric-react/lib/Modal';
+import {Link} from 'office-ui-fabric-react/lib/Link'
 
 class App extends React.Component{
 
@@ -10,7 +14,7 @@ class App extends React.Component{
         this.startWs();
         this.state = {
             connected: false,
-            lastStatus: null,
+            syncTasks: {}
         }
     }
 
@@ -63,27 +67,50 @@ class App extends React.Component{
             console.log('Correctly connected!', data)
         }  else if(data.Type === 'STATUS'){
             this.setState({lastStatus: data.Content})
+        } else if(data.Type === 'STATE'){
+            const {syncTasks} = this.state;
+            const {UUID} = data.Content;
+            syncTasks[UUID] = data.Content;
+            this.setState({syncTasks});
         } else {
             console.log(data)
         }
     }
 
+    sendMessage(type, content) {
+        this.ws.json({Type: type, Content: content});
+    }
+
     render(){
-        const {connected, lastStatus} = this.state;
-
+        const {connected, syncTasks} = this.state;
         return (
-            <div className="App">
-                <header className="App-header" style={{opacity:connected?1:0.5}}>
-                    <img src={logo} className="App-logo" alt="logo" />
-                    {lastStatus &&
-                        <p>
-                            {lastStatus.StatusString} : {lastStatus.Progress ? Math.ceil(lastStatus.Progress * 100)+'%':''}
-                        </p>
-                    }
-                </header>
-            </div>
+            <ScrollablePane style={{opacity:connected?1:.5}}>
+                <Modal
+                    titleAriaId={"Client disconnected"}
+                    isOpen={!connected}
+                    isBlocking={true}
+                >
+                    <div style={{width: 320}}>
+                        <div style={{padding: '0 20px'}}><h2>Disconnected</h2></div>
+                        <div style={{padding: '10px 20px 20px'}}>Disconnected from process! Please wait for reconnection...</div>
+                    </div>
+                </Modal>
+                <div>
+                    <Sticky stickyPosition={StickyPositionType.Header}>
+                        <div style={{backgroundColor:'#e0e0e0', padding: 20, display:'flex', alignItems:'center'}}>
+                            <div style={{flex: 1, fontSize: 20}}>All Tasks</div>
+                            <div><Link href={"http://localhost:6060/debug/pprof"} target={"_blank"}>Debugger</Link></div>
+                        </div>
+                    </Sticky>
+                    <div>
+                    {Object.keys(syncTasks).map(k => {
+                        const task = syncTasks[k];
+                        return <SyncTask key={k} state={task} sendMessage={this.sendMessage.bind(this)}/>
+                    })}
+                    </div>
+                </div>
+            </ScrollablePane>
         );
-
     }
 }
 
---
+++ app/ux/src/SyncTask.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import {ProgressIndicator} from "office-ui-fabric-react/lib/ProgressIndicator";
+import {DefaultButton} from "office-ui-fabric-react/lib/Button"
+import {Label} from "office-ui-fabric-react/lib/Label"
+import moment from 'moment'
+
+const StateSample = {
+    UUID:"e7c1cf0e-ff87-44f3-b445-bafcfcad6c0d",
+    Config:{
+        Uuid:"e7c1cf0e-ff87-44f3-b445-bafcfcad6c0d",
+        LeftURI:"router:///personal/admin/folder1",
+        RightURI:"fs:///Users/charles/Pydio/folder",
+        Direction:"Bi"
+    },
+    Connected:true,
+    Status:0,
+    LastConnection:"2019-05-03T11:54:37.772312+02:00",
+    LastSyncTime:"2019-05-03T11:54:40.775684+02:00",
+    LastProcessStatus:{
+        IsError:false,
+        StatusString:"Task Idle",
+        Progress:0
+    },
+    LeftInfo:null,
+    RightInfo:null
+};
+
+
+class SyncTask extends React.Component {
+
+
+    render() {
+
+        const {state, sendMessage} = this.props;
+        const {LastProcessStatus} = state;
+        let pg;
+        if (LastProcessStatus && LastProcessStatus.Progress) {
+            pg = LastProcessStatus.Progress;
+        }
+        return (
+            <div style={{margin:10, padding: 10, border: '1px solid #e0e0e0'}}>
+                <div>
+                    <div>
+                        <Label>Connected</Label>
+                        {state.Connected ? 'OK' : 'NO! Last connection was ' + state.LastConnection}
+                    </div>
+                    <div>
+                        <Label>Last Sync</Label>
+                        {moment(state.LastSyncTime).fromNow()}
+                    </div>
+                    <div>
+                        <Label>Status</Label>
+                        {LastProcessStatus && <div>{LastProcessStatus.StatusString}</div>}
+                        {pg && <div><ProgressIndicator title={"Progress"} description={LastProcessStatus.StatusString} percentComplete={pg}/></div>}
+                    </div>
+                    <div>
+                        <Label>Config</Label>
+                        Sync between {state.Config.LeftURI} &lt;=&gt; {state.Config.RightURI}
+                    </div>
+                </div>
+                <div style={{textAlign:'right'}}>
+
+                    <DefaultButton
+                        data-automation-id="test"
+                        allowDisabledFocus={true}
+                        disabled={false}
+                        checked={false}
+                        text="Loop"
+                        onClick={() => sendMessage('CMD', {UUID:state.UUID, Cmd:'loop'})}
+                    />
+                    &nbsp;
+                    &nbsp;
+                    <DefaultButton
+                        data-automation-id="test"
+                        allowDisabledFocus={true}
+                        disabled={false}
+                        checked={false}
+                        text="Resync"
+                        onClick={() => sendMessage('CMD', {UUID:state.UUID, Cmd:'resync'})}
+                    />
+
+                </div>
+            </div>
+        );
+
+    }
+
+}
+
+export {SyncTask as default}
