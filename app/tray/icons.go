244d4b7a7862f33eebaccd44b1d786a71c2f68ed
cdujeu <charles@ajaxplorer.info>
2019-10-17 10:23:01 +0200

    Add pause/resume plus icon to systray

--- app/tray/icons.go
+++ app/tray/icons.go
@@ -9,16 +9,19 @@ import (
 	coloractive "github.com/pydio/cells-sync/app/tray/color/iconactive"
 	coloractive2 "github.com/pydio/cells-sync/app/tray/color/iconactive2"
 	colorerror "github.com/pydio/cells-sync/app/tray/color/iconerror"
+	colorpause "github.com/pydio/cells-sync/app/tray/color/iconpause"
 
 	darkicon "github.com/pydio/cells-sync/app/tray/dark/icon"
 	darkactive "github.com/pydio/cells-sync/app/tray/dark/iconactive"
 	darkactive2 "github.com/pydio/cells-sync/app/tray/dark/iconactive2"
 	darkerror "github.com/pydio/cells-sync/app/tray/dark/iconerror"
+	darkpause "github.com/pydio/cells-sync/app/tray/dark/iconpause"
 
 	lighticon "github.com/pydio/cells-sync/app/tray/light/icon"
 	lightactive "github.com/pydio/cells-sync/app/tray/light/iconactive"
 	lightactive2 "github.com/pydio/cells-sync/app/tray/light/iconactive2"
 	lighterror "github.com/pydio/cells-sync/app/tray/light/iconerror"
+	lightpause "github.com/pydio/cells-sync/app/tray/light/iconpause"
 )
 
 var (
@@ -26,6 +29,7 @@ var (
 	iconActiveData  = coloractive.Data
 	iconActive2Data = coloractive2.Data
 	iconErrorData   = colorerror.Data
+	iconPauseData   = colorpause.Data
 )
 
 func init() {
@@ -42,11 +46,13 @@ func init() {
 			iconActiveData = lightactive.Data
 			iconActive2Data = lightactive2.Data
 			iconErrorData = lighterror.Data
+			iconPauseData = lightpause.Data
 		} else {
 			iconData = darkicon.Data
 			iconActiveData = darkactive.Data
 			iconActive2Data = darkactive2.Data
 			iconErrorData = darkerror.Data
+			iconPauseData = darkpause.Data
 		}
 	}
 }
